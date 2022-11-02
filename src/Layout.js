8cdf0bc194a52fdd9d2b743010cd077b88f3bbfa
SankaraMoothi <sankaramoorthi99@gmail.com>
2022-11-02 10:50:01 +0530

    firstCommit

---
+++ src/Layout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import { SideBar } from "./Component/Sidebar";
+import { Navbar } from "./Component/Navbar";
+
+export function Layout() {
+  return (
+    <div className="App-child">
+      <div>
+        <SideBar />
+      </div>
+      <div className="top-nav">
+        <div>
+          <Navbar />
+        </div>
+        <div className="bgColourApp">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
+}
